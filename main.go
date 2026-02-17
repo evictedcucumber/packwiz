4c4e3debc55c8baa4665df1e86aef5a4625741b5
Ethan <95688781+evictedcucumber@users.noreply.github.com>
2026-02-17 16:29:37 +0200

    feat: remove unused import types

--- main.go
+++ main.go
@@ -4,11 +4,8 @@ import (
 	// Modules of packwiz
 	"github.com/evictedcucumber/packwiz/cmd"
 	_ "github.com/evictedcucumber/packwiz/curseforge"
-	_ "github.com/evictedcucumber/packwiz/github"
 	_ "github.com/evictedcucumber/packwiz/migrate"
-	_ "github.com/evictedcucumber/packwiz/modrinth"
 	_ "github.com/evictedcucumber/packwiz/settings"
-	_ "github.com/evictedcucumber/packwiz/url"
 	_ "github.com/evictedcucumber/packwiz/utils"
 )
 
